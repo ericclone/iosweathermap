7bd7a1aa001883dba2ac97e1b1dc5019786972c2
EW <eric.clone@gmail.com>
2017-12-19 01:55:03 -0800

    Swipe to previous/next city in the list

--- WeatherApp/CityViewController.swift
+++ WeatherApp/CityViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import os.log
 
-class CityViewController: UIViewController, UITableViewDataSource {
+class CityViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
 //    func numberOfSections(in tableView: UITableView) -> Int {
 //        return 1
@@ -16,7 +16,7 @@ class CityViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let cityIndex = cityIndex {
             let count = Model.shared.cities[cityIndex].daily.count
-            print("valid cityIndex with \(count) entries ")
+            print("valid cityIndex with \(count) entries in section \(section)")
             return count
         } else {
             print("City View has no index")
@@ -25,6 +25,7 @@ class CityViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         print("DailyTableView called")
         // Table view cells are reused and should be dequeued using a cell identifier.
         
@@ -47,6 +48,21 @@ class CityViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGestures(gesture:)))
+        swipeRight.direction = .right
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGestures(gesture:)))
+        swipeLeft.direction = .left
+        
+        self.view.addGestureRecognizer(swipeRight)
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        dailyTableView.dataSource = self
+        dailyTableView.delegate = self
+        
+        updateUI()
+    }
+    
+    private func updateUI() {
         
         // Set up views if editing an existing Meal.
         if let cityIndex = cityIndex {
@@ -59,16 +75,28 @@ class CityViewController: UIViewController, UITableViewDataSource {
                 view.backgroundColor = UIColor(red: 0.8, green: 0.8, blue: 1.0, alpha: 0.8)
             }
             navigationItem.title = cityName
-            dailyTableView.dataSource = self
-//            nameTextField.text   = meal.name
-//            photoImageView.image = meal.photo
-//            ratingControl.rating = meal.rating
         }
-        
-        // Enable the Save button only if the text field has a valid Meal name.
-//        updateSaveButtonState()
     }
     
+    @objc func respondToSwipeGestures(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case .right:
+                if cityIndex != Model.shared.cities.count - 1 {
+                    cityIndex! += 1
+                }
+                print("swipe right \(String(describing: cityIndex))")
+            case .left:
+                if cityIndex != 0 {
+                    cityIndex! -= 1
+                }
+                print("swipe left \(String(describing: cityIndex))")
+            default:
+                break
+            }
+        }
+        updateUI()
+    }
     
 }
 
